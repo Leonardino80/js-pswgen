40fdcdf664748c7d654bafa29e5821daf880a59a
Leonardino80 <gilgamesh2005@gmail.com>
2019-04-16 03:10:23 +0200

    finale

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var nome = prompt('Ciao! Mi diresti il tuo nome?');
+console. log(nome)
+alert('Grazie!' + ' ' + nome);
+var cognome = prompt('Adesso scrivi per favore il tuo cognome..');
+console. log(cognome)
+var colore = prompt('ok! ... e il tuo colore preferito?');
+console. log(colore)
+document.getElementById('sorpresa').innerHTML = 'Dicevamo: questo è un generatore di password. La tua è:';
+document.writeln(nome + cognome +'19');
